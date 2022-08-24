92fdb5d38e3e12877057428db57b37dae0c5089c
ariola <ariola.markou@gmail.com>
2022-08-24 15:31:36 +0300

    Fetched the data

---
+++ src/Tour.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tour(props) {
+  return (
+    <div className="tour">
+      <h4> {props.name}</h4>
+    </div>
+  );
+}
+
+export default Tour;
--- src/Tours.js
+++ src/Tours.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Tour from "./Tour";
 import "./Tours.css";
 
 function Tours() {
+  const [tours, setTours] = useState();
+
+  const fetchTours = async () => {
+    const response = await fetch(
+      "https://course-api.com/react-tours-project"
+    ).then((response) => response.json());
+    setTours(response);
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   return (
     <div className="tours container">
       <div className="row">
         <div className="col">
-          <h2> Hello World</h2>
+          {tours.map((tour) => (
+            <div className="item-container"> {tour.name}</div>
+          ))}
+          <Tour />
         </div>
       </div>
     </div>
   );
 }
-
 export default Tours;
